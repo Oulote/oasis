ab3536a15ba500e7c5778275dd62a5659e7bb70c
Oulote <jeanmicheloulote@gmail.com>
2026-06-29 20:26:30 +0000

    Refactor WSGI config for project path inclusion
    Correction du wsgi.py pour Render

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,15 @@
 """
 WSGI config for oasis project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Ajouter le chemin du projet
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oasis.settings')
 
 application = get_wsgi_application()
